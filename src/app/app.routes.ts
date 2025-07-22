261159be819681a47852d5409fcb12b8e5a4ebb0
Larissa Faria <lari.f4ria@gmail.com>
2025-07-22 14:37:28 -0300

    created page Home and component Header; add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MinigamesComponent } from './pages/minigames/minigames.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'minigames',
+    component: MinigamesComponent
+  }
+];
